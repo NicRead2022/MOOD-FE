d3254af122cd6cd46c37c769ec3e7499e90a435f
Nicole Read <nicole.poulard@gmail.com>
2022-12-13 10:11:07 -0500

    playlistcard

--- src/components/PlaylistCard.js
+++ src/components/PlaylistCard.js
@@ -5,12 +5,38 @@ import '../CSS/Home.css'
 import { useParams, useNavigate, Link } from 'react-router-dom'
 import React from 'react'
 
-const PlaylistCard = ({}) => {
+const PlaylistCard = ({feeling, indexKey}) => {
+  let backgroundColor = '#ff7d00ff'
+   if (
+   feeling === 'Sad'
+  ) {
+   backgroundColor = '#15616dff'
+  }
+   if (
+   feeling === 'Rage'
+  ) {
+   backgroundColor = '#78290fff'
+  }
+ const playliststyle = {
+   backgroundColor: backgroundColor,
+   height: '200px',
+   width: 'calc(100vw-16px)',
+   maxWidth: '400px',
+   borderRadius: '25px',
+   margin: '8px'
  
-  return(
-    <div>{[]}</div>
-  )
-}
-
-
-export default PlaylistCard
+ 
+ 
+ }
+ 
+ 
+   return(
+     <div key= {indexKey}style={
+       playliststyle
+     }
+   ></div>
+   )
+ }
+ 
+ 
+ export default PlaylistCard
